477c778e9a938928f8afca967c8e03221042a91c
orion-tian <orionwtian@gmail.com>
2023-06-26 15:26:49 -0500

    sub branch UI done

--- snowflake.py
+++ snowflake.py
@@ -4,19 +4,30 @@ import math
 import random
 
 objName = "snowflake"
-mainBranchDefault = 6
-subdivDefault = 20
-height = 0.1
-radius = 0.2
-length = 2
+height = 0.05
+radius = 0.1
+length = 1
 scale = 0.1
 
+mainBranchDefault = 6
+subdivDefault = 20
+subBranchDefault = 3
+ls = (.1,.1,.1)
+
+posiMin = lenMin = 0
+posiMax = lenMax = 1
+angleMin = 5
+angleMax = 90
+posiStep = 0.01
+lenStep = 0.1
+anglStep = 1
+
 def ws_center(pObj):
-    bbx = cmds.xform(pObj, q=True, bb=True, ws=True)
-    centerX = (bbx[0] + bbx[3]) / 2.0
-    centerY = (bbx[1] + bbx[4]) / 2.0
-    centerZ = (bbx[2] + bbx[5]) / 2.0
-    return (centerX, centerY, centerZ)
+  bbx = cmds.xform(pObj, q=True, bb=True, ws=True)
+  centerX = (bbx[0] + bbx[3]) / 2.0
+  centerY = (bbx[1] + bbx[4]) / 2.0
+  centerZ = (bbx[2] + bbx[5]) / 2.0
+  return (centerX, centerY, centerZ)
 
 def make_sub_branches(percntAlongBranch, local_scl, len, angle):
   lx = len*math.cos(angle*math.pi/180)
@@ -42,7 +53,7 @@ def make_sub_branches(percntAlongBranch, local_scl, len, angle):
     
 
     cmds.polyExtrudeFacet(objName+'.f[%s]' %i, ld=local_dir, lt=(lx, 0, lz), 
-                            ls=local_scl, d=subdiv)
+                            ls=local_scl)#, d=subdiv/2)
 
 def random_sub_br(slider):
   for i in range(cmds.intSliderGrp(slider, q=True, value=True)):
@@ -63,12 +74,35 @@ def create(*args):
   random_sub_br(numSubBranches)
 
 def resetCallback(*pArgs):
-        # return UI to default values
-        cmds.intSliderGrp(mainBranchSlider, e=True, value=mainBranchDefault)
-        cmds.intSliderGrp(numSubdvSlider, e=True, value=subdivDefault)
+  # return UI to default values
+  cmds.intSliderGrp(mainBranchSlider, e=True, value=mainBranchDefault)
+  cmds.intSliderGrp(numSubdvSlider, e=True, value=subdivDefault)
+  cmds.intSliderGrp(numSubBranches, e=True, value=subBranchDefault)
 
-        # remake default snowflake
-        create()
+  # remake default snowflake
+  create()
+
+def create_slider(pLabel, pMin, pMax, pVal, pStep, function, show=True):
+  slider = cmds.intSliderGrp(label=pLabel, columnAlign= (1,'right'), 
+                                  field=True, min=pMin, max=pMax, value=pVal, 
+                                  step=pStep, dc = 'empty')
+  cmds.intSliderGrp(slider,  e=True, dc = partial(function), visible=show)
+
+  return slider
+
+def create_branch_controls(pLabel, pPosiVal, pLenVal, pAnglVal, function, enable=True):
+  cmds.text(label=pLabel, en=enable)
+  posiSlider = cmds.floatSliderGrp(columnAlign= (1,'right'), field=True, min=posiMin, 
+                                   max=posiMax, value=pPosiVal, step=posiStep, dc = 'empty')
+  cmds.floatSliderGrp(posiSlider,  e=True, dc = partial(function), en=enable)
+  lenSlider = cmds.floatSliderGrp(columnAlign= (1,'right'), field=True, min=lenMin, 
+                                   max=lenMax, value=pLenVal, step=lenStep, dc = 'empty')
+  cmds.floatSliderGrp(lenSlider,  e=True, dc = partial(function), en=enable)
+  angleSlider = cmds.intSliderGrp(columnAlign= (1,'right'), field=True, min=angleMin, 
+                                   max=angleMax, value=pAnglVal, step=anglStep, dc = 'empty')
+  cmds.intSliderGrp(angleSlider,  e=True, dc = partial(function), en=enable)
+
+  return (posiSlider, lenSlider, angleSlider)
 
 #-------------UI--------------------#
 windowID = "myWindowID"
@@ -81,26 +115,38 @@ cmds.columnLayout(adjustableColumn=True)
 
 # branch controls
 cmds.separator(h=10, style='none')
+mainBranchSlider = create_slider('Main Branches', 3, 12, mainBranchDefault, 1, create)
+numSubdvSlider = create_slider('Subdivision', 1, 50, subdivDefault, 1, create)
+numSubBranches = create_slider('Sub-Branches', 1, 7, subBranchDefault, 1, create)
+cmds.separator(h=5, style='out')
 
-mainBranchSlider = cmds.intSliderGrp(label='Main Branches', columnAlign= (1,'right'), 
-                                field=True, min=3, max=12, value=mainBranchDefault, 
-                                step=1, dc = 'empty')
-cmds.intSliderGrp(mainBranchSlider,  e=True, dc = partial(create))
-
-numSubdvSlider = cmds.intSliderGrp(label='Subdivisions', columnAlign= (1,'right'), 
-                                field=True, min=1, max=100, value=subdivDefault, 
-                                step=1, dc = 'empty')
-cmds.intSliderGrp(numSubdvSlider,  e=True, dc = partial(create))
-
-numSubBranches = cmds.intSliderGrp(label='Sub Branches', columnAlign= (1,'right'), 
-                                field=True, min=1, max=20, value=5, 
-                                step=1, dc = 'empty')
-cmds.intSliderGrp(numSubBranches,  e=True, dc = partial(create))
+# sub branch controls
+cmds.rowColumnLayout(numberOfColumns=4, columnWidth=[(1,130),(2, 150),(3, 150),(4, 150)])
+# headings
+cmds.separator(h=5, style='none')
+cmds.text(label='position', align='left')
+cmds.text(label='length', align='left')
+cmds.text(label='angle', align='left')
+# branches
+sub_1 = create_branch_controls('Sub-Branch 1', random.random(), random.random(), 
+                               random.randint(angleMin, angleMax), create)
+sub_2 = create_branch_controls('Sub-Branch 2', random.random(), random.random(), 
+                               random.randint(angleMin, angleMax), create)
+sub_3 = create_branch_controls('Sub-Branch 3', random.random(), random.random(), 
+                               random.randint(angleMin, angleMax), create)
+sub_4 = create_branch_controls('Sub-Branch 4', random.random(), random.random(), 
+                               random.randint(angleMin, angleMax), create, False)
+sub_5 = create_branch_controls('Sub-Branch 5', random.random(), random.random(), 
+                               random.randint(angleMin, angleMax), create, False)
+sub_6 = create_branch_controls('Sub-Branch 6', random.random(), random.random(), 
+                               random.randint(angleMin, angleMax), create, False)
+sub_7 = create_branch_controls('Sub-Branch 7', random.random(), random.random(), 
+                               random.randint(angleMin, angleMax), create, False)
 
 #buttons
-cmds.separator(h=10, style='none')
-cmds.rowColumnLayout(numberOfColumns=4, 
-                         columnWidth=[(1,225),(2, 75), (3, 10), (4,75)])
+# cmds.separator(h=10, style='none')
+cmds.setParent('..')
+cmds.rowColumnLayout(numberOfColumns=4, columnWidth=[(1,400),(2, 75), (3, 10), (4,75)])
 cmds.separator(h=5, style='none')
 cmds.button(label='Re-Create', command=partial(create))
 cmds.separator(h=5, style='none')
@@ -109,19 +155,6 @@ cmds.separator(h=5, style='none')
 
 cmds.showWindow()
   
-#-----------------Operations----------------------#
+#-----------------Main----------------------#
 create()
 cmds.select(objName)
-
-ls = (.1,.1,.1)
-
-# make_sub_branches(0.25, ls, 0.5, 90)
-# make_sub_branches(0.5, ls, 1, 85)
-# make_sub_branches(0.6, ls, 0.6, 85)
-# make_sub_branches(0.75, ls, 0.7, 60)
-# make_sub_branches(0.85, ls, 0.8, 60)
-# make_sub_branches(0.95, ls, 0.9, 60)
-
-
-
-
